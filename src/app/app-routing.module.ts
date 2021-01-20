b6b7b2254d1bb8a150d4f151720ba50eec06345b
dilejt <62393624+dilejt@users.noreply.github.com>
2021-01-20 13:29:10 +0100

    HalfIndex

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { KonkursyComponent } from './konkursy/konkursy.component';
 import { KontaktComponent } from './kontakt/kontakt.component';
 import { MenuComponent } from './menu/menu.component';
+import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
   { path: 'kontakt', component: KontaktComponent},
   { path: 'menu', component: MenuComponent},
-  { path: 'konkursy', component: KonkursyComponent}
+  { path: 'konkursy', component: KonkursyComponent},
+  { path: '', component: IndexComponent}
 ];
 
 @NgModule({

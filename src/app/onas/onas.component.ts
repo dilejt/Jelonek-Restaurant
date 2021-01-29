4f50324d183ecd78f4030f1902b386189264e374
LukaszSinica <64593019+LukaszSinica@users.noreply.github.com>
2021-01-29 18:02:12 +0100

    Animacje_2
    Dodałem animacje do reszty modułów

--- src/app/onas/onas.component.ts
+++ src/app/onas/onas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {gsap} from 'gsap'
 
+declare var $: any;
 @Component({
   selector: 'app-onas',
   templateUrl: './onas.component.html',
@@ -10,6 +12,19 @@ export class OnasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    gsap.from(".animation",{duration:1,opacity:0,x:-400,scale:0.8,ease:"power0"});
+    $(document).scroll(function()
+    {
+      checkOffset();
+    })
 
+  }
+ 
+}
+function checkOffset() {
+  if($('#header').offset().top + $('#header').height() 
+                                         >= $('#tabela').offset().top - 10)
+      $('#przycisk').css('position', 'absolute');
+  if($(document).scrollTop() + window.innerHeight < $('#stop').offset().top+80)
+      $('#przycisk').css('position', 'fixed'); // restore when you scroll up
 }

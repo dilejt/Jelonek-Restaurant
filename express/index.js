a36344879f7e67665b87d9aa1a8a52b386595e82
dilejt <62393624+dilejt@users.noreply.github.com>
2021-06-01 13:22:03 +0200

    express include

---
+++ express/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const port = 3300;
+var cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+const connection  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '',
+  database        : 'restauracja'
+});
+
+app.listen(port);
+
+app.get('', (req, res) => {
+  connection.query('SELECT d.nazwa as nazwa,d.cena as cena,d.opis as opis,d.img as img,k.nazwa as kategoria,k.img as catimg,d.idkategoria as idkategoria FROM dania d INNER JOIN kategoria k ON d.idkategoria = k.idkategoria',(err,rows)=>{
+    if (err) throw err;
+    console.log(rows);
+    res.json(rows);
+  });
+});
+
+app.post('/kontakt',function(req,res)
+{
+  let name = req.body.name;
+  let email = req.body.email;
+  let subject = req.body.subject;
+  let message = req.body.message;
+  let rating = req.body.rating;
+
+  let sql = "INSERT INTO klient(tozsamosc,temat,email,opinia,ocena) VALUES('"+name+"', '"+subject+"','"+email+"','"+message+"','"+rating+"')";
+  connection.query(sql, function (err, result) {
+      console.log("1 record inserted");
+      res.end();
+  });
+
+})
+
+app.get('/opinie', (req, res) => {
+  connection.query('SELECT tozsamosc, email, temat, opinia, ocena FROM klient',(err,rows)=>{
+    if (err) throw err;
+    console.log(rows);
+    res.json(rows);
+  });
+});
